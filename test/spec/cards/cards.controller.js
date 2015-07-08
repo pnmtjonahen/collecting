02ca89d8ccb4dc89f5610b2b179e57284d503b38
Philippe Tjon - A - Hen <philippe@tjonahen.nl>
2015-07-08 20:17:23 +0200

    added unit tests for menu

--- test/spec/cards/cards.controller.js
+++ test/spec/cards/cards.controller.js
@@ -1,99 +1,100 @@
-'use strict';
+(function () {
+    'use strict';
 
-describe('Controller: CardsController', function () {
-
-    // load the controller's module
-    beforeEach(module('collectingApp'));
-
+    var dummy1 = {
+        "id": "dummy1",
+        "name": "dummy1",
+        "content": [
+            "###Dummy",
+            "Dummy content"
+        ],
+        "htmlContent": [
+            "<html></html>"
+        ],
+        "objects": [
+            {
+                "images": [],
+                "name": "dummbyobj",
+                "content": "content",
+                "htmlContent": "content"
+            },
+            {
+                "images": [],
+                "name": "dummbyobj2",
+                "content": ["content"],
+                "htmlContent": ["content"]
+            }
+        ]
+    };
+    var dummy2 = {
+        "id": "dummy2",
+        "name": "dummy2",
+        "cards": [
+            {
+                "id": "dummy1",
+                "name": "dummy1",
+                "content": [
+                    "###Dummy",
+                    "Dummy content"
+                ],
+                "htmlContent": [
+                    "<html></html>"
+                ],
+                "objects": [
+                    {
+                        "images": [],
+                        "name": "dummbyobj",
+                        "content": "content",
+                        "htmlContent": "content"
+                    },
+                    {
+                        "images": [],
+                        "name": "dummbyobj2",
+                        "content": ["content"],
+                        "htmlContent": ["content"]
+                    }
+                ]
+            }
+        ]
+    };
     var cards_json = {
-                    "name": "dummy",
-                    "logo": "dummy",
-                    "cards": [
-                        {
-                            "id": "dummy1",
-                            "name": "dummy1",
-                            "content": [
-                                "###Dummy",
-                                "Dummy content"
-                            ],
-                            "htmlContent": [
-                                "<html></html>"
-                            ],
-                            "objects": [
-                                {
-                                    "images": [],
-                                    "name": "dummbyobj",
-                                    "content": "content",
-                                    "htmlContent": "content"
-                                },
-                                {
-                                    "images": [],
-                                    "name": "dummbyobj2",
-                                    "content": ["content"],
-                                    "htmlContent": ["content"]
-                                }
-                            ]
-                        },
-                        {
-                            "id": "dummy2",
-                            "name": "dummy2",
-                            "cards": [
-                                {
-                                    "id": "dummy1",
-                                    "name": "dummy1",
-                                    "content": [
-                                        "###Dummy",
-                                        "Dummy content"
-                                    ],
-                                    "htmlContent": [
-                                        "<html></html>"
-                                    ],
-                                    "objects": [
-                                        {
-                                            "images": [],
-                                            "name": "dummbyobj",
-                                            "content": "content",
-                                            "htmlContent": "content"
-                                        },
-                                        {
-                                            "images": [],
-                                            "name": "dummbyobj2",
-                                            "content": ["content"],
-                                            "htmlContent": ["content"]
-                                        }
-                                    ]
-                                }
-                            ]
-                        }]
-                };
-    var cardsCtrl,
-            $httpBackend,
-            createController;
+        "name": "dummy",
+        "logo": "dummy",
+        "cards": [dummy1, dummy2]
+    };
 
-    beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
-        $httpBackend = _$httpBackend_;
-        $httpBackend.when('GET', 'data/cards.json').respond(cards_json);
-        createController = function () {
-            $httpBackend.expectGET('data/cards.json');
-            cardsCtrl = $controller('CardController', {$scope: $rootScope.$new()});
-            $httpBackend.flush();
-        };
-    }));
+    describe('Controller: CardsController', function () {
 
-    afterEach(function () {
-        $httpBackend.verifyNoOutstandingExpectation();
-        $httpBackend.verifyNoOutstandingRequest();
+        // load the controller's module
+        beforeEach(module('collectingApp'));
+
+        var cardsCtrl,
+                $httpBackend,
+                createController;
+
+        beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.when('GET', 'data/cards.json').respond(cards_json);
+            createController = function () {
+                $httpBackend.expectGET('data/cards.json');
+                cardsCtrl = $controller('CardController', {$scope: $rootScope.$new()});
+                $httpBackend.flush();
+            };
+        }));
+
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should have a card with data', function () {
+            createController();
+
+            expect(cardsCtrl.cards.length).toBe(2);
+            expect(cardsCtrl.collection).not.toBeUndefined();
+
+            expect(cardsCtrl.determineMenuIcon()).toBe("menu");
+        });
+
     });
-
-    it('should have a card with data', function () {
-        createController();
-
-        expect(cardsCtrl.cards.length).toBe(2);
-        expect(cardsCtrl.collection).not.toBeUndefined();
-        
-        
-    });
-
-});
-
-
+})();
