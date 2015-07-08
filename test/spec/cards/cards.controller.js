6064f92578f399d9649e2027c356d8b3021ab153
Philippe Tjon - A - Hen <philippe@tjonahen.nl>
2015-07-08 13:34:53 +0200

    added unit tests for menu

---
+++ test/spec/cards/cards.controller.js
@@ -0,0 +1,99 @@
+'use strict';
+
+describe('Controller: CardsController', function () {
+
+    // load the controller's module
+    beforeEach(module('collectingApp'));
+
+    var cards_json = {
+                    "name": "dummy",
+                    "logo": "dummy",
+                    "cards": [
+                        {
+                            "id": "dummy1",
+                            "name": "dummy1",
+                            "content": [
+                                "###Dummy",
+                                "Dummy content"
+                            ],
+                            "htmlContent": [
+                                "<html></html>"
+                            ],
+                            "objects": [
+                                {
+                                    "images": [],
+                                    "name": "dummbyobj",
+                                    "content": "content",
+                                    "htmlContent": "content"
+                                },
+                                {
+                                    "images": [],
+                                    "name": "dummbyobj2",
+                                    "content": ["content"],
+                                    "htmlContent": ["content"]
+                                }
+                            ]
+                        },
+                        {
+                            "id": "dummy2",
+                            "name": "dummy2",
+                            "cards": [
+                                {
+                                    "id": "dummy1",
+                                    "name": "dummy1",
+                                    "content": [
+                                        "###Dummy",
+                                        "Dummy content"
+                                    ],
+                                    "htmlContent": [
+                                        "<html></html>"
+                                    ],
+                                    "objects": [
+                                        {
+                                            "images": [],
+                                            "name": "dummbyobj",
+                                            "content": "content",
+                                            "htmlContent": "content"
+                                        },
+                                        {
+                                            "images": [],
+                                            "name": "dummbyobj2",
+                                            "content": ["content"],
+                                            "htmlContent": ["content"]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }]
+                };
+    var cardsCtrl,
+            $httpBackend,
+            createController;
+
+    beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+        $httpBackend = _$httpBackend_;
+        $httpBackend.when('GET', 'data/cards.json').respond(cards_json);
+        createController = function () {
+            $httpBackend.expectGET('data/cards.json');
+            cardsCtrl = $controller('CardController', {$scope: $rootScope.$new()});
+            $httpBackend.flush();
+        };
+    }));
+
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should have a card with data', function () {
+        createController();
+
+        expect(cardsCtrl.cards.length).toBe(2);
+        expect(cardsCtrl.collection).not.toBeUndefined();
+        
+        
+    });
+
+});
+
+
