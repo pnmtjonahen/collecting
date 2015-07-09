8b7b0ddf07e6c41bec9a93d0ce30dbfacbf46e74
Philippe Tjon - A - Hen <philippe@tjonahen.nl>
2015-07-09 14:18:40 +0200

    Added gulp, and sonar-runner properties

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp");
+var karma = require("karma").server;
+var replace = require("gulp-replace");
+
+var postprocessLCOV = function() {
+    return gulp.src("reports/coverage/lcov.info")
+        .pipe(replace("SF:.", "SF:Collecting"))
+        .pipe(gulp.dest("reports/coverage"));
+};
+gulp.task("test", function () {
+    karma.start({
+        configFile: __dirname + "/karma.conf.js"
+    }, postprocessLCOV);
+});
--- karma.conf.js
+++ karma.conf.js
@@ -71,7 +71,7 @@ module.exports = function (config) {
         ],
         // Continuous Integration mode
         // if true, it capture browsers, run tests and exit
-        singleRun: false,
+        singleRun: true,
         colors: true,
         // level of logging
         // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
