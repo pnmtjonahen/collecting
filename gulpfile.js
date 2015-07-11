9cb0987ed4a974c0e8be0c2bae73d5db7bab353b
Philippe Tjon - A - Hen <philippe@tjonahen.nl>
2015-07-11 14:06:25 +0200

    Added gulp, and sonar-runner properties

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ var gulp = require("gulp"),
         karma = require("karma").server,
         replace = require("gulp-replace");
 
+var postprocessLCOV = function () {
+    return  gulp.src("reports/coverage/lcov.info")
+            .pipe(replace("SF:.", "SF:app"))
+            .pipe(gulp.dest("reports/coverage"));
+    };
 
 gulp.task("test", function () {
     karma.start({
         configFile: __dirname + "/karma.conf.js",
         singleRun: true
-    });
+    }, postprocessLCOV);
 });
 
-gulp.task("process-lcov", function () {
-    return  gulp.src("reports/coverage/lcov.info")
-            .pipe(replace("SF:.", "SF:app"))
-            .pipe(gulp.dest("reports/coverage"));
-    });
